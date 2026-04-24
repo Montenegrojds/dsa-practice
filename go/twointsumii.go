b61d55317f0a156dec8b69f976bc66343fe99795
monte <montenegrojds@gmail.com>
2026-04-23 21:33:44 -0500

    two int sum II in go

---
+++ go/twointsumii.go
@@ -0,0 +1,17 @@
+func twoSum(numbers []int, target int) []int {
+	p1 := 0
+	p2 := len(numbers)-1
+	for p1<p2{
+		sum:= numbers[p1]+numbers[p2]
+		if sum == target{
+			return []int{p1 + 1, p2 + 1}
+		}
+		if sum>target{
+			p2--
+		}
+		if sum<target{
+			p1++
+		}
+	}
+	return nil
+}
