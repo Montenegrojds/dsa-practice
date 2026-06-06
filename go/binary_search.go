db04f96bd2b759d542fed1e137f28367e46be711
monte <montenegrojds@gmail.com>
2026-06-06 09:03:15 -0500

    binary search in go

---
+++ go/binary_search.go
@@ -0,0 +1,48 @@
+/*
+Binary Search
+Easy Topics Company 
+Tags Google, Infosys, Meta, Apple, Amazon, Bloomberg, Cognizant, Microsoft, Oracle, TCS, Yandex 
+Hints
+
+You are given an array of distinct integers nums, sorted in ascending order, and an integer target.
+
+Implement a function to search for target within nums. If it exists, then return its index, otherwise, return -1.
+
+Your solution must run in O(logn)O(logn) time.
+
+Example 1:
+
+Input: nums = [-1,0,2,4,6,8], target = 4
+
+Output: 3
+
+Example 2:
+
+Input: nums = [-1,0,2,4,6,8], target = 3
+
+Output: -1
+
+Constraints:
+
+    1 <= nums.length <= 10000.
+    -10000 < nums[i], target < 10000
+    All the integers in nums are unique.
+*/
+
+func search(nums []int, target int) int {
+l:=0
+r:=len(nums)-1
+for l<=r{
+	mid:=(l+r)/2
+	if nums[mid]<target{
+		l=mid+1
+
+	}else if nums[mid]>target{
+		r=mid-1
+	}else{
+		return mid
+	}
+
+}
+return -1
+}
